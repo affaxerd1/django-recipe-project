bfe89145585a1f1d08da8eab07ee161f047c426b
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-07-09 00:59:03 +0300

    updating dockerfile and checks

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,13 +1,15 @@
 from django.test import SimpleTestCase
 
 from app import calc
+
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
     def test_subtract(self):
-        res = calc.sub(7,3)
+        res = calc.sub(7, 3)
 
         self.assertEqual(res, 4)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,21 @@
 """django command to wait for the db to be available"""
+import time
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    def handle(self, *args,**options):
-            pass
+    def handle(self, *args, **options):
+        """Entrypoint for command"""
+        self.stdout.write("waiting for database connection...")
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_ups = True
+            except:
+                self.stdout.write("Database unavailable, waiting for 1 second")
+                time.sleep(1)
+
+            self.stdout.write(self.style.SUCCESS('dATABASE AVAILABLE!'))
