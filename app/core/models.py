899c84394ccc5b02868a6f8bfeae74775a4c93ed
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-08-09 15:45:20 +0300

    require email input and add super user

--- app/core/models.py
+++ app/core/models.py
@@ -3,17 +3,29 @@ Database models
 """
 
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin,)
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 
 class UserManager(BaseUserManager):
     """Manager for users"""
     def create_user(self, email, password=None, **extra_fields):
         """create, save and return user"""
+        if not email:
+            raise ValueError('Users must have an email address')
         user=self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ user in the system"""
