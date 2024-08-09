9bcf4283460f55cccaf0114cec5bcc6f914f885a
affaxerd1 <caffaxerd@kabarak.ac.ke>
2024-08-09 14:39:11 +0300

    implement user model and normalize emails

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,13 +6,28 @@ from django.contrib.auth import get_user_model #get default user model to be ref
 
 class ModelTests(TestCase):
     """ Test creating a user with email is succesful"""
+    def test_create_user_with_email_successful(self):
 
-    email = "test@example.com"
-    password = 'testpass123'
-    user = get_user_model().objects.create_user(
-        email=email,
-        password=password
-    )
 
-    self.assertEqual(user.email, email)
-    self.assertTrue(check_password(password))
+        email = "test1@example.com"
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        ''' Test email is normalized for new user'''
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
