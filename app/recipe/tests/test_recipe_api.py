c33b2a5265c760f8c41ed18d47d6752ce8ff7cf5
affaxerd1 <ralph@affaxerd.affaxerd>
2024-09-15 17:28:08 -0400

    implementing tests for listing recipes

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -1,4 +1,3 @@
-""" Test recipe API """
 from decimal import Decimal
 from django.contrib.auth import get_user_model
 from django.test import TestCase
@@ -11,32 +10,32 @@ from core.models import Recipe
 from recipe.serializers import RecipeSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
-def create_recipe(use, **params):
-    """create and return a sample recipe"""
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe."""
     defaults = {
-        'title' : "Sample recipe title",
-        'time_minutes' : 10,
+        'title': "Sample recipe title",
+        'time_minutes': 10,
         'price': Decimal('5.25'),
         'description': 'Sample recipe description',
         'link': 'https://samplelink.com'
     }
     defaults.update(params)
 
-    recipe = Recipe.objects.create(user=user, **defaults)
-    return recipe
+    return Recipe.objects.create(user=user, **defaults)
 
 class PublicRecipeApiTests(TestCase):
-    """Test unauthorized API requests"""
+    """Test unauthorized API requests."""
     def setUp(self):
         self.client = APIClient()
 
     def test_auth_required(self):
-        """Test that auth is required to call API"""
+        """Test that auth is required to call API."""
         res = self.client.get(RECIPES_URL)
-        self.assertEqyualo(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 class PrivateRecipeApiTests(TestCase):
-    """Test authenticated API requests"""
+    """Test authenticated API requests."""
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
@@ -44,19 +43,24 @@ class PrivateRecipeApiTests(TestCase):
             'testpass'
         )
         self.client.force_authenticate(self.user)
-    def test_tetrieve_recipes(self):
-        """Test retrieving a list of recipes"""
-        create_recipe(user=self.user)
+
+    def test_retrieve_recipes(self):
+        """Test retrieving a list of recipes."""
+        other_user = get_user_model().objects.create_user(
+            'other@example.com',
+            'password123'
+        )
+        create_recipe(user=other_user)
         create_recipe(user=self.user)
 
         res = self.client.get(RECIPES_URL)
-        recipes = Recipe.objects.all().order_by('-id')
+        recipes = Recipe.objects.filter(user=self.user).order_by('-id')
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
     def test_recipes_limited_to_user(self):
-        """Test retrieving recipes for user"""
+        """Test retrieving recipes for user."""
         user2 = get_user_model().objects.create_user(
             'user1@example.com',
             'testpass123'
@@ -65,9 +69,7 @@ class PrivateRecipeApiTests(TestCase):
         recipe = create_recipe(user=self.user)
 
         res = self.client.get(RECIPES_URL)
-        recipes = Recipe.objects.filter(user=self.user)
+        recipes = Recipe.objects.filter(user=self.user).order_by('-id')
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(res.data), serializer.data)
-
-
+        self.assertEqual(len(res.data), len(serializer.data))
